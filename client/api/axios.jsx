819d26f2eb2774f43a29cfd9c393c76c91c935e4
vanamuthuV <vanamuthuvana22@gmail.com>
2024-08-30 18:21:49 +0530

    U[dates

--- client/api/axios.jsx
+++ client/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const BASE_URL = "http://localhost:5000";
 const BASE_URL =
-  "https://attendance-3t3n7aw73-vanamuthu-vs-projects.vercel.app/";
+  "https://attendance-3t3n7aw73-vanamuthu-vs-projects.vercel.app";
 
 export default axios.create({
   baseURL: BASE_URL,
